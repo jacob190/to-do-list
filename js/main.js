e7784a5a8489091afa7d5e56be7bd99c5b9fc614
Jakub Cybulak <jakub.cybulak@gmail.com>
2019-10-28 18:32:39 +0100

    function changes

--- js/main.js
+++ js/main.js
@@ -1,54 +1,62 @@
-const tasksListArr = [];
+function startList() {
 
-const form = document.querySelector('.list-form');
-const taskInput = document.querySelector('.list-form__input');
-const addTaskBtn = document.querySelector('.list-form__btn');
-const searchTaskInput = document.querySelector('.list-form__search');
-const tasksList = document.querySelector('.tasks-list');
-const searchInput = document.querySelector('.list-form__search')
+    const tasksListArr = [];
+
+    const form = document.querySelector('.list-form');
+    const taskInput = document.querySelector('.list-form__input');
+    const addTaskBtn = document.querySelector('.list-form__btn');
+    const searchTaskInput = document.querySelector('.list-form__search');
+    const tasksList = document.querySelector('.tasks-list');
+    const searchInput = document.querySelector('.list-form__search')
+
+    const renderTasksList = (tasksArr) => {
+        tasksList.textContent = "";
+        tasksArr.forEach((arrElement, keyIndex) => {
+            arrElement.dataset.key = keyIndex;
+            tasksList.appendChild(arrElement);
+        })
+    }
+
+    const deleteTask = (e) => {
+        e.target.parentNode.remove();
+        const index = e.target.parentNode.dataset.key
+        tasksListArr.splice(index, 1)
+        renderTasksList(tasksListArr);
+    }
 
 
-const renderTasksList = (arr) => {
-    tasksList.textContent = "";
-    arr.forEach((arrElement, keyIndex) => {
-        arrElement.dataset.key = keyIndex;
-        tasksList.appendChild(arrElement);
-    })
+    const addTask = (e) => {
+        e.preventDefault();
+
+        const taskTitle = taskInput.value;
+        if (taskTitle === "") return;
+
+        const task = document.createElement('li');
+        task.className = 'task';
+        task.innerHTML = taskTitle + ' <button>x</button>';
+        tasksListArr.push(task)
+        renderTasksList(tasksListArr);
+
+
+        tasksList.appendChild(task);
+        taskInput.value = "";
+
+        task.querySelector('button').addEventListener('click', deleteTask);
+
+    }
+
+    const searchTask = (e) => {
+        const searchTxt = e.target.value.toLowerCase();
+        const filteredTasksListArr = tasksListArr.filter(task => task.textContent.toLowerCase().includes(searchTxt));
+        renderTasksList(filteredTasksListArr);
+    }
+
+
+    form.addEventListener('submit', addTask);
+    searchInput.addEventListener('keyup', searchTask);
+
+
 }
 
-const deleteTask = (e) => {
-    e.target.parentNode.remove();
-    const index = e.target.parentNode.dataset.key
-    tasksListArr.splice(index, 1)
-    renderTasksList(tasksListArr);
-}
 
-
-const addTask = (e) => {
-    e.preventDefault();
-
-    const taskTitle = taskInput.value;
-    if (taskTitle === "") return;
-
-    const task = document.createElement('li');
-    task.className = 'task';
-    task.innerHTML = taskTitle + ' <button>x</button>';
-    tasksListArr.push(task)
-    renderTasksList(tasksListArr);
-
-    tasksList.appendChild(task);
-    taskInput.value = "";
-
-    task.querySelector('button').addEventListener('click', deleteTask);
-
-}
-
-const searchTask = (e) => {
-    const searchTxt = e.target.value.toLowerCase();
-    const filteredTasksListArr = tasksListArr.filter(task => task.textContent.toLowerCase().includes(searchTxt));
-    renderTasksList(filteredTasksListArr);
-}
-
-
-form.addEventListener('submit', addTask);
-searchInput.addEventListener('keyup', searchTask);
+window.addEventListener(onload, startList());
